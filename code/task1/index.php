b6eced0d9b6b49e5d7388358ed3df5cfdaf41a12
Miran Ul Haq <68357661+mnuh96@users.noreply.github.com>
2022-04-01 13:41:42 -0400

    Create index.php

---
+++ code/task1/index.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <title>
+      Task 1
+    </title>
+  <meta name="description" content="My Name is Miran Ul Haq and this is my web programming assignment 2.">
+  </head>
+  <body>
+    <form method="post" action="login.php">
+      <p>Username: <input type="text" name="name"</p>
+      <p>Password: <input type="password" name="password"</p>
+      <p><input type="submit" value="Let me in"</p>
+    </form>
+  </body>
+</html>
